cf952a459812b97f4151f9baca9ca3e990355679
Salih Citak <salihcitak@yahoo.com>
2020-05-06 16:24:15 -0400

    event listener and handler

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,11 +6,19 @@ function Start()
     firstHeading.textContent = "Goodbye Cruel World";
     /*document.getElementById("firstHeading").textContent = "Goodbye Cruel World";*/
     firstHeading.style.fontWeight= "bold";
-    let main=getElementsByTagName("main")[0];
+    
     let newH2= document.createElement("h2");
     newH2.setAttribute("class","Display-5");
     newH2.textContent="Second Heading";
-    main.appendChild(newH2);
+    firstHeading.appendChild(newH2);
+    newH2.addEventListener("click", function(){
+        newH2.style.color = (newH2.style.color="black")? "blue":"black";
+    });
+    newH2.addEventListener("mouseover", function(){
+        newH2.style.color="green";
+    });
+    newH2.addEventListener("mouseout",function(){
+        newH2.style.color="black"});
 
     
 }
