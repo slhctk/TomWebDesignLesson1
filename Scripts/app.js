5d7ec831073c75e25dca3b3ed76c006314be31d9
Salih Citak <salihcitak@yahoo.com>
2020-05-05 20:18:34 -0400

    added bootstrap and font-awesome

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,6 +2,16 @@
 function Start()
 {
     console.log("App started...");
+    let firstHeading= document.getElementById("firstHeading");
+    firstHeading.textContent = "Goodbye Cruel World";
+    /*document.getElementById("firstHeading").textContent = "Goodbye Cruel World";*/
+    firstHeading.style.fontWeight= "bold";
+    let main=getElementsByTagName("main")[0];
+    let newH2= document.createElement("h2");
+    newH2.setAttribute("class","Display-5");
+    newH2.textContent="Second Heading";
+    main.appendChild(newH2);
+
     
 }
 window.addEventListener("load", Start);
